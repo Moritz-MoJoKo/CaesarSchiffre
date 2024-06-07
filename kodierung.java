117fb38930a4e0c4dacbf472fe9739ebe296f7cb
Moritz MoJoKo <100783353+Moritz-MoJoKo@users.noreply.github.com>
2024-06-07 08:56:20 +0200

    generell edit

--- kodierung.java
+++ kodierung.java
@@ -18,16 +18,4 @@ public class kodierung
         // Instanzvariable initialisieren
         x = 0;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
 }
